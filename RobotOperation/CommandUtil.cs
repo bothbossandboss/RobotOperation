02c6d5027a7d51ec679ba4d2f82823a01ec13bf7
bothboss <bothboss21@gmail.com>
2015-04-10 06:32:30 +0900

    サーボの角度指令を作成するクラスを追加。

---
+++ RobotOperation/CommandUtil.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * ロボットの各関節の回転角を指定するクラス。
+ * 城さんの作ったクラスをそのままコピー。
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotOperation {
+    class CommandUtil {
+        public static int servoAngleConvert(double angle) {
+            //7500のとき0deg            
+            int neutral = 7500;       
+            //±135deg =&gt;±4000 
+            int servoAngle = neutral + (int)((angle / 135.0) * 4000);
+
+            //サーボモーター最大角、最小角の設定
+            //3500より小さいときは3500にする
+            if (servoAngle < 3500) servoAngle = 3500;      
+
+            //11500より大きいときは11500にする
+            if (servoAngle > 11500) servoAngle = 11500;     
+
+            return servoAngle; 
+        }
+
+        public static byte[] SeriesServoMove(Dictionary<int, int> dest, int frame) {
+            int cnt = dest.Count();
+            byte size = (byte)(8 + 2 * cnt + 1);
+            byte[] cmd = new byte[size];
+            cmd[0] = size;
+            cmd[1] = 0x10;
+            cmd[7] = (byte)frame;
+
+            int selectIndex = 2;
+            int destIndex = 8;
+            int servoCnt = 0;
+            foreach (int key in dest.Keys) {
+                int quot = (int)(key / 8);
+                int offset = key % 8;
+                cmd[selectIndex + quot] += (byte)(System.Math.Pow(2, offset));
+
+                byte[] destByte = BitConverter.GetBytes((short)dest[key]);
+                cmd[destIndex + servoCnt * 2] = destByte[0];
+                cmd[destIndex + servoCnt * 2 + 1] = destByte[1];
+                servoCnt++;
+            }
+
+            byte sum = 0;
+            for (int i = 0; i < size - 1; i++) {
+                sum += cmd[i];
+            }
+            cmd[size - 1] = sum;
+            return cmd;
+        }
+
+    }
+}
--- RobotOperation/Program.cs
+++ RobotOperation/Program.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RobotOperation
-{
-    static class Program
-    {
+namespace RobotOperation {
+    static class Program {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+        static void Main() {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Console.WriteLine("Hello World using C#!");
         }
     }
 }
