1f8be78f8e43e4be5d8b7e0b0cc19e8b2db9e48b
bothboss <bothboss21@gmail.com>
2015-04-10 06:43:53 +0900

    不要なForm1ファイルを削除。

--- RobotOperation/Program.cs
+++ RobotOperation/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*
+ * アプリケーションのメイン関数を含むクラス。
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,14 +9,8 @@ using System.Windows.Forms;
 
 namespace RobotOperation {
     static class Program {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
         [STAThread]
         static void Main() {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
             Console.WriteLine("Hello World using C#!");
         }
     }
