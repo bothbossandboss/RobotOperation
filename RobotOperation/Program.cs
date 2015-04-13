aeea30246dd0c9866666c8aa8f7b68761958ba14
bothboss <bothboss21@gmail.com>
2015-04-13 11:48:32 +0900

    微小歩行の遷移を表現。

--- RobotOperation/Program.cs
+++ RobotOperation/Program.cs
@@ -16,17 +16,121 @@ namespace RobotOperation {
             SmallWalk smallWalk = new SmallWalk();
             ServoManager servoManager = new ServoManager();
             string nowPos = "posFirst";
-            Dictionary<ServoTag, double> preD;
-            Dictionary<ServoTag, double> nextD;
-
-            while(true) {
+            Dictionary<ServoTag, double> nowD = smallWalk.posDic["posFirst"];
+            Dictionary<ServoTag, double> nextD = smallWalk.posDic["pos13"];
+            int frame = smallWalk.frameDic["pos13"];
+            int count = 6;
+            bool flag = true;
+            //15msごとに指令をロボットに送る。
+            while(flag) {
+                //現在のポジションと次のポジションを設定
                 switch (nowPos) {
                     case "posFirst":
-                        preD = smallWalk.posDic["posFirst"];
+                        nowD = smallWalk.posDic["posFirst"];
                         nextD = smallWalk.posDic["pos13"];
+                        frame = smallWalk.frameDic["pos13"];
+                        nowPos = "pos13";
+                        break;
+                    case "pos13":
+                        nowD = smallWalk.posDic["pos13"];
+                        nextD = smallWalk.posDic["pos45"];
+                        frame = smallWalk.frameDic["pos45"];
+                        nowPos = "pos45";
+                        break;
+                    case "pos45":
+                        nowD = smallWalk.posDic["pos45"];
+                        nextD = smallWalk.posDic["pos7"];
+                        frame = smallWalk.frameDic["pos7"];
+                        nowPos = "pos7";
+                        break;
+                    case "pos7":
+                        nowD = smallWalk.posDic["pos7"];
+                        nextD = smallWalk.posDic["pos9"];
+                        frame = smallWalk.frameDic["pos9"];
+                        nowPos = "pos9";
+                        break;
+                    case "pos9":
+                        nowD = smallWalk.posDic["pos9"];
+                        nextD = smallWalk.posDic["pos33"];
+                        frame = smallWalk.frameDic["pos33"];
+                        nowPos = "pos33";
+                        break;
+                    case "pos33":
+                        nowD = smallWalk.posDic["pos33"];
+                        --count;
+                        if (count == 0) {
+                            nextD = smallWalk.posDic["pos43"];
+                            frame = smallWalk.frameDic["pos43"];
+                            nowPos = "pos43";
+                        }else {
+                            nextD = smallWalk.posDic["pos0"];
+                            frame = smallWalk.frameDic["pos0"];
+                            nowPos = "pos0";
+                        }
+                        break;
+                    case "pos0":
+                        nowD = smallWalk.posDic["pos0"];
+                        nextD = smallWalk.posDic["pos8"];
+                        frame = smallWalk.frameDic["pos8"];
+                        nowPos = "pos8";
+                        break;
+                    case "pos8":
+                        nowD = smallWalk.posDic["pos8"];
+                        nextD = smallWalk.posDic["pos22"];
+                        frame = smallWalk.frameDic["pos22"];
+                        nowPos = "pos22";
+                        break;
+                    case "pos22":
+                        nowD = smallWalk.posDic["pos22"];
+                        nextD = smallWalk.posDic["pos4"];
+                        frame = smallWalk.frameDic["pos4"];
+                        nowPos = "pos4";
+                        break;
+                    case "pos4":
+                        nowD = smallWalk.posDic["pos4"];
+                        --count;
+                        if (count == 0) {
+                            nextD = smallWalk.posDic["pos44"];
+                            frame = smallWalk.frameDic["pos44"];
+                            nowPos = "pos44";
+                        }else {
+                            nextD = smallWalk.posDic["pos1"];
+                            frame = smallWalk.frameDic["pos1"];
+                            nowPos = "pos1";
+                        }
+                        break;
+                    case "pos1":
+                        nowD = smallWalk.posDic["pos1"];
+                        nextD = smallWalk.posDic["pos7"];
+                        frame = smallWalk.frameDic["pos7"];
+                        nowPos = "pos7";
+                        break;
+                    case "pos43":
+                        nowD = smallWalk.posDic["pos43"];
+                        nextD = smallWalk.posDic["posFin"];
+                        frame = smallWalk.frameDic["posFin"];
+                        nowPos = "posFin";
+                        break;
+                    case "pos44":
+                        nowD = smallWalk.posDic["pos44"];
+                        nextD = smallWalk.posDic["posFin"];
+                        frame = smallWalk.frameDic["posFin"];
+                        nowPos = "posFin";
+                        break;
+                    case "posFin":
+                        flag = false;
+                        break;
+                    default:
                         break;
                 }
+                //コマンドを生成してロボットに送信
+                for (int i = 1; i <= frame && flag; i++) {
+                    servoManager.setLowerBody(ref nowD, ref nextD, (double)i / (double)frame);
+                    servoManager.generateCommand();
+                    System.Threading.Thread.Sleep(15);
+                }
 
+                break;
             }
 
 
